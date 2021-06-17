a5e56d0991c789224677291f892d92cc45ffb192
Babrakoskei <80739366+Babrakoskei@users.noreply.github.com>
2021-06-17 11:08:04 +0300

    Add files via upload

---
+++ python_test.py
@@ -0,0 +1,87 @@
+#Question1
+x = [100,110,120,130,140]
+newlist = []
+
+for a in x:
+    newlist.append(a * 5)
+
+print(newlist)
+
+
+#question2
+def divisible_by_three(n):
+    for num in range(n):
+         if num % 3 == 0:
+              print(f"{num} is divisible by 3")
+         else:
+             print(f"{num} is not divisible by 3")  
+
+divisible_by_three(100)  
+
+#Question3
+x = [[1,2],[3,4],[5,6]]
+def flatlist(xList):
+    if len(xList) == 0:
+        return xList
+    if isinstance(xList[0], list):
+        return flatlist(xList[0]) + flatlist(xList[1:])
+    return xList[:1] + flatlist(xList[1:])
+
+
+print(flatlist([[1,2],[3,4],[5,6]]))
+
+#Question4
+def smallest():
+    list1 = [3,6,8,2,4,1,5,7]
+    list1.sort()
+    print( list1[:1])
+
+smallest()
+
+#question5
+x= ['a','b','a','e','d','b','c','e','f','g','h']
+y = {'a','b','a','e','d','b','c','e','f','g','h'}
+print(y)
+
+
+#Question6
+def divisible_by_seven():
+    for r in range(100,200):
+        if r%7==0:
+            print(f"{r} is divisible by seven")
+        else:
+            print(f"{r} is not divisible by seven")
+
+divisible_by_seven()                
+
+
+
+#question7
+ 
+
+def greetings(name,age):
+    
+    students = [
+    {"age": 19, "name": "Eunice"},
+     {"age": 21, "name": "Agnes"},
+    {"age": 18, "name": "Teresa"},
+    {"age": 22, "name": "Asha"}
+    ]
+
+
+
+
+#Question8
+class Rectangle:
+    def __init__(self,width,height):
+        self.width= width
+        self.height= height
+
+    def area(self): 
+        return    
+
+
+        
+ 
+
+
